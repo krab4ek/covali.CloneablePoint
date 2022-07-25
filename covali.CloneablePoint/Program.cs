662ef4786f45c85072cb6d2b51b76ca399f6efe0
Covali Andrei <krab4ek@gmail.com>
2022-07-25 20:12:23 +0300

    Add project files.

---
+++ covali.CloneablePoint/Program.cs
@@ -0,0 +1,11 @@
+﻿using covali.CloneablePoint;
+
+
+Console.WriteLine("****** Fun with Object Cloning *****\n");
+Point p1 = new Point(50, 50);
+Point p2 = p1;
+p2.X = 0;
+
+Console.WriteLine(p1);
+Console.WriteLine(p2);
+Console.WriteLine();
